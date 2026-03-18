0f108521d5909d4e996a948389cc60edd875d231
Lebogang Sekaleli <lebogangsekaleli@gmail.com>
2026-03-18 15:20:38 +0200

    Rating state

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
+
 const Rating = () => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+  
+
   const stars = Array.from({ length: 5 }, (_, i) => i + 1);
+
   return (
     <div className="rating-container">
       <h2>Rate Your Experience</h2>
       <div className="stars">
         {stars.map((star) => (
-          <span key={star} className="star">
+          <span 
+          onClick={() => setRating(star)} 
+          onMouseEnter={() => setHover(star)}
+          onMouseLeave={() => setHover(0)}
+          key={star} className="star">
             {"\u2605"}
           </span>
         ))}
